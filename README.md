67efe00fd23c3b2ef0fb7b24b3e549538457ec13
exihs <brydonti@gmail.com>
2024-05-16 17:13:25 +0800

    completed home page + navbar stuff
    navbar: added jump link for how it works

---
+++ README.md
@@ -0,0 +1,9 @@
+Node modules:
+express
+nodemon
+
+To start:
+run 
+npm start
+or
+node <path to app.js>
--- src/public/scripts/common.js
+++ src/public/scripts/common.js
@@ -1,2 +1,5 @@
 //load the navbar html into the class
 $(".nav-placeholder").load("./commonHTML/navbar.html")
+
+//load the footer html into the class
+$(".footer-placeholder").load("./commonHTML/footer.html")
