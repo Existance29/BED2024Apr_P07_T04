1239b695a1ef409b4f7260791c02a44553b48ab4
exihs <brydonti@gmail.com>
2024-05-27 20:18:54 +0800

    added dbconfig + package installations
    Installed mssql and connect to database

---
+++ src/database/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "bed-assg-1", 
+    password: "bed-assg-1", 
+    server: "localhost",
+    database: "bed-assg-1",
+    trustServerCertificate: true,
+    options: {
+      port: 1433, 
+      connectionTimeout: 60000, 
+    },
+  };
