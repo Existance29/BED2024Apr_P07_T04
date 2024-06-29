db44aac6846785856afec8243a8491467568200e
Koh Shan Chun <s10243956@connect.np.edu.sg>
2024-06-29 17:26:59 +0800

    Adding commentController.js, commentRoute.js

---
+++ src/controllers/commentController.js
@@ -0,0 +1,43 @@
+const Comment = require("../models/comment");
+
+const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.getAllComments();
+        res.json(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error retrieving comments");
+    }
+}
+
+const createComment = async (req, res) => {
+    const newComment = req.body;
+    try {
+        const createdComment = await Comment.createComment(newComment);
+        res.status(201).json(createdComment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error creating comment");
+    }
+}
+
+const editComment = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedData = req.body;
+    try {
+        const editedComment = await Comment.updateComment(id, updatedData);
+        if (!editedComment) {
+            return res.status(404).send("Comment not found");
+        }
+        res.json(editedComment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error editing comment");
+    }
+}
+
+module.exports = {
+    createComment,
+    getAllComments,
+    editComment,
+};
