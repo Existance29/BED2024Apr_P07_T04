1540b8e3a69e7bc7465d40dd75fada8aef0b637d
exihs <brydonti@gmail.com>
2024-07-15 13:44:49 +0800

    some quiz routes documentation
    excluded the routes that rely on userid

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -1,6 +1,20 @@
 const Quiz = require("../models/quiz");
 
 const getAllQuizzes = async (req, res) => {
+    // #swagger.tags = ['Quizzes']
+    // #swagger.description = 'Get a list of all quizzes'
+    /* #swagger.responses[200] = {
+                description: 'Success, returns an array of all quiz objects.',
+                schema: [{
+                    id: 1,
+                    title: "Angular JS Basics",
+                    description: "Test your knowledge on the basics of Angular JS.",
+                    totalQuestions: 5,
+                    totalMarks: 50,
+                    duration: 1,
+                    maxAttempts: 2
+                }]
+        } */
     try {
         const quizzes = await Quiz.getAllQuizzes();
         res.json(quizzes);
@@ -11,6 +25,25 @@ const getAllQuizzes = async (req, res) => {
 }
 
 const getQuizById = async (req, res) => {
+    // #swagger.tags = ['Quizzes']
+    // #swagger.description = 'Get a quiz by its id'
+    /*  #swagger.parameters['quizId'] = {
+          in: 'path',
+          type: "int",
+          description: 'The id of the quiz',
+    } */
+   /* #swagger.responses[200] = {
+                description: 'Success, returns the quiz object',
+                schema: {
+                    id: 1,
+                    title: "Angular JS Basics",
+                    description: "Test your knowledge on the basics of Angular JS.",
+                    totalQuestions: 5,
+                    totalMarks: 50,
+                    duration: 1,
+                    maxAttempts: 2
+                }
+        } */
     const id = parseInt(req.params.quizId);
     try {
         const quiz = await Quiz.getQuizById(id);
@@ -25,6 +58,28 @@ const getQuizById = async (req, res) => {
 }
 
 const getQuizQuestions = async (req, res) => {
+    // #swagger.tags = ['Quizzes']
+    // #swagger.description = 'Get a list of all questions associated to a quiz'
+    /*  #swagger.parameters['quizId'] = {
+          in: 'path',
+          type: "int",
+          description: 'The id of the quiz',
+    } */
+    /* #swagger.responses[200] = {
+                description: 'Success, returns an array of all quiz questions.',
+                schema: [{
+                    id: 1,
+                    quizId: 1,
+                    text: "What is Angular JS?",
+                    options: [
+                        "A framework",
+                        "A library",
+                        "A language",
+                        "An IDE"
+                    ],
+                    correctAnswer: 0
+                }]
+        } */
     const quizId = req.params.quizId;
     try {
         const questions = await Quiz.getQuizQuestions(quizId);
@@ -36,6 +91,7 @@ const getQuizQuestions = async (req, res) => {
 };
 
 const submitQuizAnswers = async (req, res) => {
+    
     const quizId = parseInt(req.params.quizId);
     const { userId, answers, duration } = req.body;  // Ensure userId is included in the request body
     try {
