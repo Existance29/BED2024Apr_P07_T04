276c0696382463e2d306c3b0ca87852b0dd64698
exihs <54537576+Existance29@users.noreply.github.com>
2024-07-23 11:00:07 +0800

    more quizController tests

--- src/tests/quizController.test.js
+++ src/tests/quizController.test.js
@@ -188,58 +188,125 @@ describe("quizController.submitQuizAnswers", () => {
   beforeEach(() => {
     jest.clearAllMocks(); // Clear mock calls before each test
   });
+
+  //mock request body
   const req = {
+    params: {quizId: 1},
+    user: {userId: 1},
     body: {
-      title: 'Xcode',
-      description: 'Learn how to use xcode with the swift programming language to develop IOS mobile apps',
-      details: 'In this course, you will learn the basics of the swift programming language, make and design elements with xcode and launch an app',
-      caption: 'Lead by industry professionals to master IOS app development',
-      category: 'programming language,app development,sofware'
-    },
-    files: {
-        
+      "answers": [
+        {
+          "questionId": 1,
+          "answer": 0
+        },
+        {
+          "questionId": 2,
+          "answer": 0
+        },
+        {
+          "questionId": 3,
+          "answer": 0
+        },
+        {
+          "questionId": 4,
+          "answer": 0
+        },
+        {
+          "questionId": 5,
+          "answer": 1
+        }
+      ],
+      "duration": 10
     }
   }
 
-  it("should create a course and return status code 201", async () => {
-    const mockCourse = {
-        courseID: 9,
-        title: 'Xcode',
-        thumbnail: null,
-        description: 'Learn how to use xcode with the swift programming language to develop IOS mobile apps',
-        details: 'In this course, you will learn the basics of the swift programming language, make and design elements with xcode and launch an app',
-        caption: 'Lead by industry professionals to master IOS app development',
-        category: 'programming language,app development,sofware',
-        totalRate: 0,
-        ratings: 0,
-        video: null,
+  it("should create a new result for the user and return the json object", async () => {
+    
+    const mockResult = {
+      "score": 50,
+      "totalQuestions": 5,
+      "correctAnswers": 5,
+      "incorrectQuestions": [],
+      "resultId": 9,
+      "grade": "A+"
     }
-    Course.createCourse.mockResolvedValue(mockCourse) // update is successful
+
+    Quiz.submitQuizAnswers.mockResolvedValue(mockResult) //mock quiz results
 
     const res = {
-      status: jest.fn().mockReturnThis(),
       json: jest.fn()
     };
 
-    await(await courseController.createCourse(req, res))
+    await quizController.submitQuizAnswers(req, res)
 
-    expect(res.status).toHaveBeenCalledWith(201);
-    expect(res.json).toHaveBeenCalledWith(mockCourse);
+    expect(res.json).toHaveBeenCalledWith(mockResult);
   });
 
 
   it("should handle errors and return a 500 status with error message", async () => {
     const errorMessage = "Database error";
-    Course.createCourse.mockRejectedValue(new Error(errorMessage)); // Simulate an error
+    Quiz.submitQuizAnswers.mockRejectedValue(new Error(errorMessage)); // Simulate an error
 
     const res = {
       status: jest.fn().mockReturnThis(),
-      json: jest.fn()
+      send: jest.fn()
     };
 
-    await courseController.createCourse(req, res);
+    await quizController.submitQuizAnswers(req, res);
 
     expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({ message: "Error creating course", error: errorMessage});
+    expect(res.send).toHaveBeenCalledWith("Error submitting quiz answers");
+  })
+})
+
+describe("quizController.getQuizResult", () => {
+  beforeEach(() => {
+    jest.clearAllMocks(); // Clear mock calls before each test
   });
-});
+
+  it("should fetch a quiz by its id and return a JSON response", async () => {
+    const mockQuiz = {
+        "id": 1,
+        "title": "Angular JS Basics",
+        "description": "Test your knowledge on the basics of Angular JS.",
+        "totalQuestions": 5,
+        "totalMarks": 50,
+        "duration": 1,
+        "maxAttempts": 2
+      }
+
+    // Mock the quiz.getAllQuizzes function to return the mock data
+    Quiz.getQuizById.mockResolvedValue(mockQuiz);
+
+    //mock the reqs
+    const req = {
+      params: {quizId: 1}
+    };
+    const res = {
+      json: jest.fn(), // Mock the res.json function
+    };
+
+    await quizController.getQuizById(req, res);
+    expect(Quiz.getQuizById).toHaveBeenCalledTimes(1); // Check if getAllQuizzes was called
+    expect(res.json).toHaveBeenCalledWith(mockQuiz); // Check the response body
+  });
+
+  it("should handle errors and return a 500 status with error message", async () => {
+    const errorMessage = "Database error";
+    Quiz.getQuizById.mockRejectedValue(new Error(errorMessage)); // Simulate an error
+
+    //mock the reqs
+    const req = {
+      params: {quizId: 1}
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    }
+
+    await quizController.getQuizById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith("Error retrieving quiz");
+  })
+})
