7e4b0b5a2d21f74252d14cd73997377301337fa5
exihs <54537576+Existance29@users.noreply.github.com>
2024-07-24 12:51:07 +0800

    quiz model test setup

--- src/tests/quiz.test.js
+++ src/tests/quiz.test.js
@@ -1,130 +1,56 @@
 // book.test.js
-const Book = require("../models/book");
+const Quiz = require("../models/quiz");
 const sql = require("mssql");
 
 jest.mock("mssql"); // Mock the mssql library
 
-describe("Book.getAllBooks", () => {
+describe("Quiz.getAllQuizzes", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should retrieve all books from the database", async () => {
-    const mockBooks = [
-        {
-            "book_id": 1,
-            "title": "The Adventures of Huckleberry Finn",
-            "author": "Mark Twain",
-            "availability": "N"
-        },
-        {
-            "book_id": 2,
-            "title": "The Scarlet Letter",
-            "author": "Nathaniel Hawthorne",
-            "availability": "Y"
-        },
-        {
-            "book_id": 3,
-            "title": "David Copperfield",
-            "author": "\tCharles Dickens",
-            "availability": "N"
-        }
-    ];
+  it("should retrieve all quizzes from the database", async () => {
+    const mockQuizzes = [
+      {
+        "id": 1,
+        "title": "Angular JS Basics",
+        "description": "Test your knowledge on the basics of Angular JS.",
+        "totalQuestions": 5,
+        "totalMarks": 50,
+        "duration": 1,
+        "maxAttempts": 2
+      },
+      {
+        "id": 2,
+        "title": "Python Basics",
+        "description": "Test your knowledge on the fundamentals of Python.",
+        "totalQuestions": 5,
+        "totalMarks": 50,
+        "duration": 100,
+        "maxAttempts": 2
+      }
+    ]
 
     const mockRequest = {
-      query: jest.fn().mockResolvedValue({ recordset: mockBooks }),
+      query: jest.fn().mockResolvedValue({ recordset: mockQuizzes }),
     };
     const mockConnection = {
       request: jest.fn().mockReturnValue(mockRequest),
       close: jest.fn().mockResolvedValue(undefined),
     };
 
-    sql.connect.mockResolvedValue(mockConnection); // Return the mock connection
+    //sql.connect.mockResolvedValue(mockConnection); // Return the mock connection
 
-    const books = await Book.getAllBooks();
+    const quizzes = await Quiz.getAllQuizzes();
 
     expect(sql.connect).toHaveBeenCalledWith(expect.any(Object));
-    expect(mockConnection.close).toHaveBeenCalledTimes(1);
-    expect(books).toHaveLength(mockBooks.length);
-    for (let i = 0; i < mockBooks.length; i++){
-        expect(books[i]).toEqual(mockBooks[i])
-    }
+    expect(mockConnection.close).toHaveBeenCalledTimes(0);
+    expect(quizzes).toHaveLength(2);
   });
 
   it("should handle errors when retrieving books", async () => {
     const errorMessage = "Error in book.js Could not get all books";
     sql.connect.mockRejectedValue(new Error(errorMessage));
     await expect(Book.getAllBooks()).rejects.toThrow(errorMessage);
-  });
-});
-
-// book.test.js (continue in the same file)
-describe("Book.updateBookAvailability", () => {
-    // ... mock mssql and other necessary components
-    beforeEach(() => {
-        jest.clearAllMocks();
-      });
-    
-  
-    it("should update the availability of a book", async () => {
-        const mockBooks = [
-            {
-                "book_id": 1,
-                "title": "The Adventures of Huckleberry Finn",
-                "author": "Mark Twain",
-                "availability": "N"
-            }
-        ];
-        jest
-        //const spy = jest.spyOn(Book, 'getBookById');
-        //spy.mockResolvedValue(mockBooks);
-
-        const mockRequest = {
-          query: jest.fn().mockResolvedValue({ rowsAffected: 1, recordset: mockBooks }),
-          input: jest.fn().mockResolvedValue(undefined),
-        };
-        const mockConnection = {
-          request: jest.fn().mockReturnValue(mockRequest),
-          close: jest.fn().mockResolvedValue(undefined),
-        };
-    
-        sql.connect.mockResolvedValue(mockConnection); // Return the mock connection
-        
-        const book = await Book.updateBookAvailability(1, "Y");
-    
-        expect(sql.connect).toHaveBeenCalledWith(expect.any(Object));
-        expect(mockConnection.close).toHaveBeenCalledTimes(2);
-        expect(book.availability).toBe('N')
-    });
-  
-    it("should return null if book with the given id does not exist", async () => {
-        const mockBooks = []
-        jest
-        //const spy = jest.spyOn(Book, 'getBookById');
-        //spy.mockResolvedValue(mockBooks);
-
-        const mockRequest = {
-          query: jest.fn().mockResolvedValue({ rowsAffected: 0, recordset: mockBooks }),
-          input: jest.fn().mockResolvedValue(undefined),
-        };
-        const mockConnection = {
-          request: jest.fn().mockReturnValue(mockRequest),
-          close: jest.fn().mockResolvedValue(undefined),
-        };
-    
-        sql.connect.mockResolvedValue(mockConnection); // Return the mock connection
-        
-        const book = await Book.updateBookAvailability(1, "Y");
-    
-        expect(sql.connect).toHaveBeenCalledWith(expect.any(Object));
-        expect(mockConnection.close).toHaveBeenCalledTimes(2);
-        expect(book).toBe(null)
-    });
-  
-    // Add more tests for error scenarios (e.g., database error)
-    it("should handle errors when updating books", async () => {
-        const errorMessage = "Error in book.js Could not update book availability";
-        sql.connect.mockRejectedValue(new Error(errorMessage));
-        await expect(Book.updateBookAvailability()).rejects.toThrow(errorMessage);
-      });
-  });
+  })
+})
