7ec7a215ac0105c963e3872e48910f999135e881
Existance29 <brydonti@gmail.com>
2024-07-24 21:26:41 +0800

    some quiz model tests

--- src/models/quiz.js
+++ src/models/quiz.js
@@ -17,6 +17,7 @@ class Quiz {
     }
 
     static toQuestionObj(row) {
+        console.log(row.options)
         return {
             id: row.id,
             quizId: row.quizId,
--- src/tests/quiz.test.js
+++ src/tests/quiz.test.js
@@ -1,8 +1,5 @@
 // book.test.js
 const Quiz = require("../models/quiz");
-const sql = require("mssql");
-
-jest.mock("mssql"); // Mock the mssql library
 
 describe("Quiz.getAllQuizzes", () => {
   beforeEach(() => {
@@ -31,26 +28,156 @@ describe("Quiz.getAllQuizzes", () => {
       }
     ]
 
-    const mockRequest = {
-      query: jest.fn().mockResolvedValue({ recordset: mockQuizzes }),
-    };
-    const mockConnection = {
-      request: jest.fn().mockReturnValue(mockRequest),
-      close: jest.fn().mockResolvedValue(undefined),
-    };
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn()
+    Quiz.query.mockResolvedValue({recordset: mockQuizzes})
 
-    //sql.connect.mockResolvedValue(mockConnection); // Return the mock connection
+    const quizzes = await Quiz.getAllQuizzes(); //get the output from the test function
 
-    const quizzes = await Quiz.getAllQuizzes();
-
-    expect(sql.connect).toHaveBeenCalledWith(expect.any(Object));
-    expect(mockConnection.close).toHaveBeenCalledTimes(0);
-    expect(quizzes).toHaveLength(2);
+    //check the results
+    expect(quizzes).toHaveLength(2)
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(quizzes[0]).toEqual(mockQuizzes[0])
+    expect(quizzes[1]).toEqual(mockQuizzes[1])
   });
 
-  it("should handle errors when retrieving books", async () => {
-    const errorMessage = "Error in book.js Could not get all books";
-    sql.connect.mockRejectedValue(new Error(errorMessage));
-    await expect(Book.getAllBooks()).rejects.toThrow(errorMessage);
+  it("should return an empty array when there are no quizzes", async () => {
+    const mockQuizzes = []
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn()
+    Quiz.query.mockResolvedValue({recordset: mockQuizzes})
+
+    const quizzes = await Quiz.getAllQuizzes() //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(quizzes).toHaveLength(0)
+    expect(quizzes).toEqual([])
+  });
+})
+
+describe("Quiz.getQuizById", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should retrieve the quiz from the database", async () => {
+    const mockQuizzes = [
+      {
+        "id": 1,
+        "title": "Angular JS Basics",
+        "description": "Test your knowledge on the basics of Angular JS.",
+        "totalQuestions": 5,
+        "totalMarks": 50,
+        "duration": 1,
+        "maxAttempts": 2
+      },
+      {
+        "id": 2,
+        "title": "Python Basics",
+        "description": "Test your knowledge on the fundamentals of Python.",
+        "totalQuestions": 5,
+        "totalMarks": 50,
+        "duration": 100,
+        "maxAttempts": 2
+      }
+    ]
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn()
+    Quiz.query.mockResolvedValue({recordset: mockQuizzes})
+
+    const quiz = await Quiz.getQuizById(1); //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(quiz).toEqual(mockQuizzes[0])
+  });
+
+  it("should return null when quiz id does not exist", async () => {
+    const mockQuizzes = []
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn()
+    Quiz.query.mockResolvedValue({recordset: mockQuizzes})
+
+    const quiz = await Quiz.getQuizById() //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(quiz).toBeNull()
+  });
+})
+
+describe("Quiz.getQuizQuestions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should retrieve the questions of a quiz from the database", async () => {
+    const mockData = [
+      {
+        "id": 1,
+        "quizId": 1,
+        "text": "What is Angular JS?",
+        "options": [
+          "A framework",
+          "A library",
+          "A language",
+          "An IDE"
+        ],
+        "correctAnswer": 0
+      }
+    ]
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn().mockResolvedValue({recordset: mockData})
+    JSON.parse = jest.fn().mockImplementationOnce(() => mockData[0].options) //also mock json.parse
+    const result = await Quiz.getQuizQuestions(1); //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(result[0]).toEqual(mockData[0])
+  });
+
+  it("should return an empty array when quiz id does not exist", async () => {
+    const mockData = []
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn().mockResolvedValue({recordset: mockData})
+
+    const result = await Quiz.getQuizQuestions(0) //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(result).toHaveLength(0)
+    expect(result).toEqual([])
+  });
+})
+
+describe("Quiz.submitQuizAnswers", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("submit the quiz answers to the database", async () => {
+    const mockResult = {
+      "score": 50,
+      "totalQuestions": 5,
+      "correctAnswers": 5,
+      "incorrectQuestions": [],
+      "resultId": 9,
+      "grade": "A+"
+    }
+
+    //mock the query to return the mssql results
+    Quiz.query = jest.fn().mockResolvedValue({recordset: mockData})
+    JSON.parse = jest.fn().mockImplementationOnce(() => mockData[0].options) //also mock json.parse
+    const result = await Quiz.getQuizQuestions(1); //get the output from the test function
+
+    //check the results
+    expect(Quiz.query).toHaveBeenCalled()
+    expect(result[0]).toEqual(mockData[0])
   })
 })
